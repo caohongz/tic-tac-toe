bd5b5c848e1a3349cf8f1bc0d90b9e287e1fcede
vue-study <18511283565@163.com>
2020-06-08 17:10:27 +0800

    try(can't get state value)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Game from "./App";
+import { Provider } from "react-redux";
+import store from "./store/";
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,42 @@
 import { createStore, combineReducers } from "redux";
 
-export const tictactoeReducer = (state, { type, payload }) => {
+// const gameState = {
+//   history: [
+//     {
+//       squares: [null, null, null, null, null, null, null, null, null],
+//       step: -1,
+//     },
+//   ],
+//   lists: [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 4, 8],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [2, 4, 6],
+//   ],
+//   count: 0,
+//   i: -1,
+//   xIsNext: true,
+//   stepNumber: 0,
+// };
+const taskListData = {
+  loading: true,
+  error: false,
+  taskList: [],
+};
+
+export const tictactoeReducer = (
+  state = { taskListData },
+  // state = { count: 0 },
+  { type }
+) => {
   switch (type) {
-    case "ADD":
-      return state + payload;
+    case "SHOW":
+      return { ...state };
   }
 };
 
-export default store = createStore(combineReducers({ home: tictactoeReducer }));
+const store = createStore(tictactoeReducer);
+export default store;
